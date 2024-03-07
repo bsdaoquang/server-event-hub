4f044eb776c78f9613b7226a3170c79a40c43c25
bsdaoquang <bsdaoquang@gmail.com>
2024-03-07 15:13:38 +0700

    get events followed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@
 
 const asyncHandle = require('express-async-handler');
 const UserModel = require('../models/userModel');
+const { query } = require('express');
+const EventModel = require('../models/eventModel');
 
 const getAllUsers = asyncHandle(async (req, res) => {
 	const users = await UserModel.find({});
@@ -21,4 +23,24 @@ const getAllUsers = asyncHandle(async (req, res) => {
 	});
 });
 
-module.exports = { getAllUsers };
+const getEventsFollowed = asyncHandle(async (req, res) => {
+	const { uid } = req.query;
+
+	if (uid) {
+		const events = await EventModel.find({ followers: { $all: uid } });
+
+		const ids = [];
+
+		events.forEach((event) => ids.push(event.id));
+
+		res.status(200).json({
+			message: 'fafa',
+			data: ids,
+		});
+	} else {
+		res.sendStatus(401);
+		throw new Error('Missing uid');
+	}
+});
+
+module.exports = { getAllUsers, getEventsFollowed };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,14 @@
 /** @format */
 
 const Router = require('express');
-const { getAllUsers } = require('../controllers/userController');
+const {
+	getAllUsers,
+	getEventsFollowed,
+} = require('../controllers/userController');
 
 const userRouter = Router();
 
 userRouter.get('/get-all', getAllUsers);
+userRouter.get('/get-followed-events', getEventsFollowed);
 
 module.exports = userRouter;
