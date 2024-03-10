03c2cc2365d9875f5841d80772fa3b5639d364f9
bsdaoquang <bsdaoquang@gmail.com>
2024-03-10 14:42:34 +0700

    add send notification function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,7 @@ const asyncHandle = require('express-async-handler');
 const UserModel = require('../models/userModel');
 const { query } = require('express');
 const EventModel = require('../models/eventModel');
+const http = require('http');
 
 const getAllUsers = asyncHandle(async (req, res) => {
 	const users = await UserModel.find({});
@@ -16,6 +17,8 @@ const getAllUsers = asyncHandle(async (req, res) => {
 			id: item.id,
 		})
 	);
+
+	await handleSendNotification();
 
 	res.status(200).json({
 		message: 'Get users successfully!!!',
@@ -51,9 +54,51 @@ const updateFcmToken = asyncHandle(async (req, res) => {
 	});
 
 	res.status(200).json({
-		message: 'fafa',
+		message: 'Fcmtoken updated',
 		data: [],
 	});
 });
+
+const handleSendNotification = async () => {
+	var request = require('request');
+	var options = {
+		method: 'POST',
+		url: 'https://fcm.googleapis.com/fcm/send',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization:
+				'key=AAAAYQVzHAg:APA91bHeOlIP2Ga6OdcOp3_UVnfqSNA32Ddum6-bbj3VFyfA32WGlaZfZ13qLrV6nz20H7k81X0GOy1Y2Qp6LAqFHrfhNB3E8tm9cFG4f2KJ2ehWdFA70PmYALvs1HS0whcyKmtIdpdk',
+		},
+		body: JSON.stringify({
+			registration_ids: [
+				'fqkKacfqSFWvJnFBEVsUDT:APA91bGffTunAovCwcdgQuB0eu4dAYrd5S6qVkImlzr96P-7SKasvte2tY3cPCdgqsn82jE1PReOOdcecXFosDBYUnI5L5pazJgPDhMlJjNym1RMHYXhPa4S_qXWFFMO_0koqLPp2DQi',
+			],
+			notification: {
+				title: 'title',
+				subtitle: 'sub title',
+				body: 'content of message',
+				sound: 'default',
+			},
+			contentAvailable: 'true',
+			priority: 'high',
+			apns: {
+				payload: {
+					aps: {
+						contentAvailable: 'true',
+					},
+				},
+				headers: {
+					'apns-push-type': 'background',
+					'apns-priority': '5',
+					'apns-topic': '',
+				},
+			},
+		}),
+	};
+	request(options, function (error, response) {
+		if (error) throw new Error(error);
+		console.log(response.body);
+	});
+};
 
 module.exports = { getAllUsers, getEventsFollowed, updateFcmToken };
