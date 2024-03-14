f57689349710a04a64e56b04ba678e639f252e0d
bsdaoquang <bsdaoquang@gmail.com>
2024-03-14 10:07:31 +0700

    fix error login with google

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -175,13 +175,32 @@ const handleLoginWithGoogle = asyncHandle(async (req, res) => {
 	const userInfo = req.body;
 
 	const existingUser = await UserModel.findOne({ email: userInfo.email });
-	let user = { ...userInfo };
+	let user;
 	if (existingUser) {
 		await UserModel.findByIdAndUpdate(existingUser.id, {
-			...userInfo,
 			updatedAt: Date.now(),
 		});
+		user = { ...existingUser };
 		user.accesstoken = await getJsonWebToken(userInfo.email, userInfo.id);
+
+		if (user) {
+			const data = {
+				accesstoken: user.accesstoken,
+				id: existingUser._id,
+				email: existingUser.email,
+				fcmTokens: existingUser.fcmTokens,
+				photo: existingUser.photoUrl,
+				name: existingUser.name,
+			};
+
+			res.status(200).json({
+				message: 'Login with google successfully!!!',
+				data,
+			});
+		} else {
+			res.sendStatus(401);
+			throw new Error('fafsf');
+		}
 	} else {
 		const newUser = new UserModel({
 			email: userInfo.email,
@@ -189,14 +208,26 @@ const handleLoginWithGoogle = asyncHandle(async (req, res) => {
 			...userInfo,
 		});
 		await newUser.save();
-
+		user = { ...newUser };
 		user.accesstoken = await getJsonWebToken(userInfo.email, newUser.id);
-	}
 
-	res.status(200).json({
-		message: 'Login with google successfully!!!',
-		data: { ...user, id: existingUser._id },
-	});
+		if (user) {
+			res.status(200).json({
+				message: 'Login with google successfully!!!',
+				data: {
+					accesstoken: user.accesstoken,
+					id: user._id,
+					email: user.email,
+					fcmTokens: user.fcmTokens,
+					photo: user.photoUrl,
+					name: user.name,
+				},
+			});
+		} else {
+			res.sendStatus(401);
+			throw new Error('fafsf');
+		}
+	}
 });
 
 module.exports = {
