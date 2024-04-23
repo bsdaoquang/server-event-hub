ecdcc85a3913e546667991cfcc6dbf9bf17e32ed
bsdaoquang <bsdaoquang@gmail.com>
2024-04-23 19:24:56 +0700

    fix server

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 const authRouter = require('./src/routers/authRouter');
 const connectDB = require('./src/configs/connectDb');
 const errorMiddleHandle = require('./src/middlewares/errorMiddleware');
+const userRouter = require('./src/routers/userRouter');
+const verifyToken = require('./src/middlewares/VerifyMiddleware');
+const eventRouter = require('./src/routers/eventRouter');
+const testRouter = require('./src/routers/plantaRouter');
 const app = express();
 require('dotenv').config();
 
@@ -13,6 +17,10 @@ app.use(express.json());
 const PORT = 3001;
 
 app.use('/auth', authRouter);
+app.use('/test', testRouter);
+
+app.use('/users', verifyToken, userRouter);
+app.use('/events', verifyToken, eventRouter);
 
 connectDB();
 
